7c4f289f965983039c6a8fc3094a5514a6cfff80
Avik Jain <avikjain12345@gmail.com>
2016-06-25 16:14:32 -0700

    Connect to firebase and basic API setup

---
+++ api/router.js
@@ -0,0 +1,29 @@
+// API Router
+var config = require('../config');
+var path = require('path');
+var firebase = require('firebase');
+var express = require('express');
+
+var router = express.Router();
+
+firebase.initializeApp({
+  serviceAccount: path.join(__dirname, '..', config.FIREBASE_CREDENTIALS_PATH),
+  databaseURL: config.FIREBASE_URL
+});
+
+var firebaseAppsRef = firebase.database().ref('apps')
+
+router.get('/:username/:appName', function(req, res) {
+  firebaseAppsRef.once('value', function(appsDataSnapshot) {
+    var apps = appsDataSnapshot.val();
+    for(var i = 0; i < apps.length; i++) {
+      if(apps[i].username == req.params.username && apps[i].appName == req.params.appName) {
+        res.json(apps[i]);
+        return;
+      }
+    }
+    res.sendStatus(404);
+  })
+});
+
+module.exports = router;
--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-var firebase = require("firebase");
+var config = require('./config');
+var path = require('path');
+var express = require('express');
 
-firebase.initializeApp({
-  serviceAccount: "credentials/ProtoDraw-8343e29cc965.json",
-  databaseURL: "https://protodraw-c64e7.firebaseio.com/"
+var apiRouter = require('./api/router');
+
+var app = express();
+
+app.set('json spaces', 2);
+
+app.use(express.static('public'));
+app.use('/api', apiRouter);
+
+app.get('/app', function(req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'app.html'));
 });
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.listen(config.PORT, config.HOST, function(err) {
+  if(err) {
+    return console.error(err);
+  }
+  console.log('app listening on port 3000');
+})
+
